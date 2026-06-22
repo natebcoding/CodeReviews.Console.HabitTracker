f221853115f049defef88f6fb3b45de0317f9ccc
GraveGhostDev <134028654+natebcoding@users.noreply.github.com>
2026-06-22 18:38:29 -0400

    CRUD Functionality Complete

--- HabitTracker/HabitTracker/DatabaseHelper.cs
+++ HabitTracker/HabitTracker/DatabaseHelper.cs
@@ -28,30 +28,6 @@ public class DatabaseHelper
     
     // Methods (Just using basic for now, need to update to DB methods)
 
-    public string CreateHabit()
-    {
-        
-        Console.WriteLine("Enter the Habit Name: ");
-        string? habitName = Console.ReadLine();
-        Console.WriteLine("Enter the Habit Quantity: ");
-        int habitQuantity = int.Parse(Console.ReadLine());
-        Console.WriteLine("""Date for the habit? Enter "today" for today, or another date.""");
-        string? habitDate = Console.ReadLine();
-
-        var habit = new Dictionary<string, object>
-        {
-            { "habitName", habitName },
-            { "habitQuantity", habitQuantity },
-            { "date", habitDate },
-        };
-        
-        
-        string createQuery = @"INSERT INTO Habits (habitName, habitQuantity, date) VALUES ($habitName, $habitQuantity, $habitDate)";
-        
-        Execute(createQuery, habit);
-        
-        return "Habit created";
-    }
 
     public void Execute(string sql, Dictionary<string, object> parameters)
     {
@@ -74,10 +50,147 @@ public class DatabaseHelper
             command.ExecuteNonQuery();
         }
     }
+    
+    public void ExecuteNoParams(string sql)
+    {
+        string connectionString = "DataSource=habitTracker.db";
+        
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+
+            command.CommandText = sql;
+            
+
+            command.ExecuteNonQuery();
+        }
+    }
+    
+    // CRUD Methods
+    
+    public string CreateHabit()
+    {
+        
+        Console.WriteLine("Enter the Habit Name: ");
+        string? habitName = Console.ReadLine();
+        Console.WriteLine("Enter the Habit Quantity: ");
+        int habitQuantity = int.Parse(Console.ReadLine());
+        Console.WriteLine("""Date for the habit? Enter "today" for today, or another date.""");
+        string? habitDate = Console.ReadLine();
+        if (habitDate == "today")
+        {
+            habitDate = Convert.ToString(DateTime.Now);
+        }
+
+        var habit = new Dictionary<string, object>
+        { // input variables are transformed to the placeholder ones for SQL query -- habitDate -> $date
+            { "$habitName", habitName },
+            { "$habitQuantity", habitQuantity },
+            { "$date", habitDate },
+        };
+        
+        
+        string createQuery = @"INSERT INTO Habits (habitName, habitQuantity, date) VALUES ($habitName, $habitQuantity, $date)"; // The $ values must match the dictionary names $date -> $date here
+        
+        Execute(createQuery, habit);
+        
+        return "Habit created";
+    }
 
     public string UpdateHabit()
     {
+        string updateQuery = "";
+        Console.WriteLine("Enter the ID of the habit you would like to update: ");
+        int targetHabitId = int.Parse(Console.ReadLine());
+        Console.WriteLine("What would you like to update on this habit?");
+        Console.WriteLine("1. Habit Name");
+        Console.WriteLine("2. Habit Quantity");
+        Console.WriteLine("3. Habit Date");
+        string? userInp = Console.ReadLine();
+
+        switch (userInp)
+        {
+            case "1": // Update Habit Name
+                Console.WriteLine("Enter the new habit name: ");
+                string? newHabitName = Console.ReadLine();
+                updateQuery = @"UPDATE Habits SET habitName = $newHabitName WHERE habitId = $targetHabitId";
+                var updateNameParams = new Dictionary<string, object>
+                    { { "$targetHabitId", targetHabitId }, { "$newHabitName", newHabitName ?? "Default Placeholder" } };
+                Execute(updateQuery, updateNameParams);
+                break;
+            case "2": // Update Habit Quantity
+                Console.WriteLine("Enter the new habit quantity: ");
+                int newHabitQuantity = int.Parse(Console.ReadLine() ?? "0");
+                updateQuery = @"UPDATE Habits SET habitQuantity = $newHabitQuantity WHERE habitId = $targetHabitId";
+                var updateQuantityParams = new Dictionary<string, object>
+                    { { "$targetHabitId", targetHabitId }, { "$newHabitQuantity", newHabitQuantity} };
+                Execute(updateQuery, updateQuantityParams);
+                break;
+            case "3": // Update Habit Date
+                Console.WriteLine("What is the new date (MM/dd/yy)?");
+                string newHabitDate = Console.ReadLine();
+                updateQuery = @"UPDATE Habits SET date = $newHabitDate WHERE habitId = $targetHabitId";
+                var updateDateParams = new Dictionary<string, object>
+                    { { "$targetHabitId", targetHabitId }, { "$newHabitDate", newHabitDate} };
+                Execute(updateQuery, updateDateParams);
+                break;
+            default:
+                Console.WriteLine("Incorrect option");
+                break;
+        }
+        
         return "Habit Updated";
+    }
+
+    public string DeleteHabit()
+    {
+        string query = @"DELETE FROM Habits WHERE habitId = $targetHabitId";
+        
+        Console.WriteLine("Enter the ID of the habit you would like to delete: ");
+        int targetHabitId = int.Parse(Console.ReadLine());
+        var deleteParams = new Dictionary<string, object> { { "$targetHabitId", targetHabitId } };
+        Execute(query, deleteParams);
+
+        return "Habit Deleted";
+    }
+    
+
+
+    public void ExecuteReader(string sql)
+    {
+        string connectionString = "DataSource=habitTracker.db";
+        
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+
+            using (var command = new SqliteCommand(sql, connection))
+            {
+                using (SqliteDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int habitId = reader.GetInt32(0);
+                        string habitName = reader.GetString(1);
+                        int habitQuantity = Convert.ToInt32(reader["habitQuantity"]);
+                        string date = reader.GetString(3);
+
+                        Console.WriteLine($"ID: {habitId}, Habit Name: {habitName}, Quantity: {habitQuantity}, Date: {date}");
+                    }
+                }
+            }
+            
+        }
+        
+    }
+
+    public void GetAllHabits()
+    {
+        string query = @"SELECT * FROM Habits";
+        
+        ExecuteReader(query);
     }
     
 }
--- HabitTracker/HabitTracker/Program.cs
+++ HabitTracker/HabitTracker/Program.cs
@@ -20,17 +20,30 @@ class Program
             Console.WriteLine("2. Update a Habit Record");
             Console.WriteLine("3. Delete a Habit Record");
             Console.WriteLine("4. View Current Habits");
+            Console.WriteLine("5. Quit");
 
             string input = Console.ReadLine();
             switch (input)
             {
                 case "1":
                     databaseHelper.CreateHabit();
-                    return;
-                case "2":
-                    Console.WriteLine("Goodbye.");
                     break;
+                case "2":
+                    databaseHelper.UpdateHabit();
+                    break;
+                case "3":
+                    databaseHelper.DeleteHabit();
+                    break;
+                case "4":
+                    databaseHelper.GetAllHabits();
+                    break;
+                case "5":
+                    Console.WriteLine("Goodbye.");
+                    isOn = false;
+                    break;
+               
                 default:
+                    Console.WriteLine("Incorrect input");
                     break;
             }
         }
