479845f94c5b5276f5c5fe55f9174c6990125c50
GraveGhostDev <134028654+natebcoding@users.noreply.github.com>
2026-06-27 14:10:06 -0400

    Challenges & Bug Fixes

--- HabitTracker/HabitTracker/DatabaseHelper.cs
+++ HabitTracker/HabitTracker/DatabaseHelper.cs
@@ -18,10 +18,29 @@ public class DatabaseHelper
 
             tableCommand.CommandText = 
                 @"CREATE TABLE IF NOT EXISTS Habits(
-                    habitId INTEGER PRIMARY KEY AUTOINCREMENT, habitName TEXT, habitQuantity INTEGER, date TEXT)";
+                    habitId INTEGER PRIMARY KEY AUTOINCREMENT, habitName TEXT, habitUnit TEXT, habitQuantity INTEGER, date TEXT)";
+            
 
             tableCommand.ExecuteNonQuery();
             
+            string existsQuery = @"SELECT COUNT(*) FROM Habits";
+            var count = ExecuteScalarNoParams(existsQuery);
+            if (count == 0)
+            {
+                var seedData = new Dictionary<string, object>
+                { // input variables are transformed to the placeholder ones for SQL query -- habitDate -> $date
+                    { "$habitName", "Drink Water"},
+                    { "$habitUnit", "glasses"},
+                    { "$habitQuantity", 5 },
+                    { "$date", "06/24/26" },
+                };
+        
+        
+                string createQuery = @"INSERT INTO Habits (habitName, habitUnit, habitQuantity, date) VALUES ($habitName, $habitUnit, $habitQuantity, $date)"; 
+        
+                Execute(createQuery, seedData);
+            
+            }
         }
 
     }
@@ -49,6 +68,49 @@ public class DatabaseHelper
             command.ExecuteNonQuery();
         }
     }
+    public int ExecuteScalar(string sql, Dictionary<string, object> parameters)
+    {
+        int count = 0;
+        string connectionString = "DataSource=habitTracker.db";
+        
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+
+            command.CommandText = sql;
+
+            foreach (var param in parameters)
+            {
+                command.Parameters.AddWithValue(param.Key, param.Value);
+            }
+
+            object result = command.ExecuteScalar();
+            count = result != DBNull.Value ? Convert.ToInt32(result) : 0;
+        }
+
+        return count;
+    }
+    public int ExecuteScalarNoParams(string sql)
+    {
+        int count = 0;
+        string connectionString = "DataSource=habitTracker.db";
+        
+        using (var connection = new SqliteConnection(connectionString))
+        {
+            connection.Open();
+
+            var command = connection.CreateCommand();
+
+            command.CommandText = sql;
+
+            object result = command.ExecuteScalar();
+            count = result != DBNull.Value ? Convert.ToInt32(result) : 0;
+        }
+
+        return count;
+    }
     
     public void ExecuteNoParams(string sql)
     {
@@ -71,21 +133,32 @@ public class DatabaseHelper
     
     public string CreateHabit()
     {
+        string? habitName = string.Empty;
+        while (string.IsNullOrEmpty(habitName))
+        {
+            Console.WriteLine("Enter the Habit Name: ");
+            habitName = Console.ReadLine();
+            
+        }
+
+        string? habitUnit = string.Empty;
+        while (string.IsNullOrEmpty(habitUnit))
+        {
+            Console.WriteLine("Enter the unit you are tracking (miles, reps, etc):");
+            habitUnit = Console.ReadLine();
+        }
         
-        Console.WriteLine("Enter the Habit Name: ");
-        string? habitName = Console.ReadLine();
         Console.WriteLine("Enter the Habit Quantity: ");
         int habitQuantity = 0;
 
-        if (int.TryParse(Console.ReadLine(), out int habitQuantityResult))
+        while (!int.TryParse(Console.ReadLine(), out habitQuantity))
         {
-            habitQuantity = habitQuantityResult;
+            Console.WriteLine("Habit Quantity must be greater than 0.");
             
         }
         Console.WriteLine("""Date for the habit? Enter "today" for today, or another date. (Format: MM/dd/yyyy)""");
         string? habitDate = Console.ReadLine();
         string dateFormat = "MM/dd/yyyy";
-
         if (habitDate == "today")
         {
             habitDate = DateTime.Now.ToString("MM/dd/yyyy");
@@ -107,12 +180,13 @@ public class DatabaseHelper
         var habit = new Dictionary<string, object>
         { // input variables are transformed to the placeholder ones for SQL query -- habitDate -> $date
             { "$habitName", habitName },
+            { "$habitUnit", habitUnit},
             { "$habitQuantity", habitQuantity },
             { "$date", habitDate },
         };
         
         
-        string createQuery = @"INSERT INTO Habits (habitName, habitQuantity, date) VALUES ($habitName, $habitQuantity, $date)"; // The $ values must match the dictionary names $date -> $date here
+        string createQuery = @"INSERT INTO Habits (habitName, habitUnit, habitQuantity, date) VALUES ($habitName, $habitUnit, $habitQuantity, $date)"; // The $ values must match the dictionary names $date -> $date here
         
         Execute(createQuery, habit);
         
@@ -122,55 +196,93 @@ public class DatabaseHelper
     public string UpdateHabit()
     {
         string updateQuery = "";
+        
         Console.WriteLine("Enter the ID of the habit you would like to update: ");
         int targetHabitId = 0;
-        if (int.TryParse(Console.ReadLine(), out int idResult))
+        if (int.TryParse(Console.ReadLine(), out int targetHabitIdresult))
         {
-            targetHabitId = idResult;
-        }
-
-        Console.WriteLine("What would you like to update on this habit?");
-        Console.WriteLine("1. Habit Name");
-        Console.WriteLine("2. Habit Quantity");
-        Console.WriteLine("3. Habit Date");
-        string? userInp = Console.ReadLine();
-
-        switch (userInp)
-        {
-            case "1": // Update Habit Name
-                Console.WriteLine("Enter the new habit name: ");
-                string? newHabitName = Console.ReadLine();
-                updateQuery = @"UPDATE Habits SET habitName = $newHabitName WHERE habitId = $targetHabitId";
-                var updateNameParams = new Dictionary<string, object>
-                    { { "$targetHabitId", targetHabitId }, { "$newHabitName", newHabitName ?? "Default Placeholder" } };
-                Execute(updateQuery, updateNameParams);
-                break;
-            case "2": // Update Habit Quantity
-                Console.WriteLine("Enter the new habit quantity: ");
-                int newHabitQuantity = 0;
-                if (int.TryParse(Console.ReadLine(), out int newHabitQuantityResult)) // Do null check, output as result
-                {
-                    newHabitQuantity = newHabitQuantityResult;
-                }
-                updateQuery = @"UPDATE Habits SET habitQuantity = $newHabitQuantity WHERE habitId = $targetHabitId";
-                var updateQuantityParams = new Dictionary<string, object>
-                    { { "$targetHabitId", targetHabitId }, { "$newHabitQuantity", newHabitQuantity} };
-                Execute(updateQuery, updateQuantityParams);
-                break;
-            case "3": // Update Habit Date
-                Console.WriteLine("What is the new date (MM/dd/yy)?");
-                string newHabitDate = Console.ReadLine();
-                updateQuery = @"UPDATE Habits SET date = $newHabitDate WHERE habitId = $targetHabitId";
-                var updateDateParams = new Dictionary<string, object>
-                    { { "$targetHabitId", targetHabitId }, { "$newHabitDate", newHabitDate} };
-                Execute(updateQuery, updateDateParams);
-                break;
-            default:
-                Console.WriteLine("Incorrect option");
-                break;
+            targetHabitId = targetHabitIdresult;
+            
         }
         
-        return "Habit Updated";
+        string existsQuery = @"SELECT COUNT(*) FROM Habits WHERE habitId = $targetHabitId";
+        var deleteParams = new Dictionary<string, object> { { "$targetHabitId", targetHabitId } };
+        var count = ExecuteScalar(existsQuery, deleteParams);
+        if (count == 1)
+        {
+            Console.WriteLine("What would you like to update on this habit?");
+            Console.WriteLine("1. Habit Name");
+            Console.WriteLine("2. Habit Unit");
+            Console.WriteLine("3. Habit Quantity");
+            Console.WriteLine("4. Habit Date");
+            string? userInp = Console.ReadLine();
+
+            switch (userInp)
+            {
+                case "1": // Update Habit Name
+                    Console.WriteLine("Enter the new habit name: ");
+                    string? newHabitName = Console.ReadLine();
+                    updateQuery = @"UPDATE Habits SET habitName = $newHabitName WHERE habitId = $targetHabitId";
+                    var updateNameParams = new Dictionary<string, object>
+                        { { "$targetHabitId", targetHabitId }, { "$newHabitName", newHabitName ?? "Default Placeholder" } };
+                    Execute(updateQuery, updateNameParams);
+                    break;
+                case "2": // Update Habit Unit
+                    Console.WriteLine("Enter the new Habit unit ");
+                    string? newHabitUnit = Console.ReadLine();
+                    updateQuery = @"UPDATE Habits SET habitUnit = $newHabitUnit WHERE habitId = $targetHabitId";
+                    var updateHabitUnitParams = new Dictionary<string, object>
+                        { { "$targetHabitId", targetHabitId }, { "$newHabitUnit", newHabitUnit ?? "Default Placeholder" } };
+                    Execute(updateQuery, updateHabitUnitParams);
+                    break;
+                case "3": // Update Habit Quantity
+                    Console.WriteLine("Enter the new habit quantity: ");
+                    int newHabitQuantity = 0;
+                    if (int.TryParse(Console.ReadLine(), out int newHabitQuantityResult)) // Do null check, output as result
+                    {
+                        newHabitQuantity = newHabitQuantityResult;
+                    }
+                    updateQuery = @"UPDATE Habits SET habitQuantity = $newHabitQuantity WHERE habitId = $targetHabitId";
+                    var updateQuantityParams = new Dictionary<string, object>
+                        { { "$targetHabitId", targetHabitId }, { "$newHabitQuantity", newHabitQuantity} };
+                    Execute(updateQuery, updateQuantityParams);
+                    break;
+                case "4": // Update Habit Date
+                    Console.WriteLine("What is the new date (MM/dd/yyyy) or today?");
+                    string? newHabitDate = Console.ReadLine();
+                    if (newHabitDate == "today")
+                    {
+                        newHabitDate = DateTime.Now.ToString("MM/dd/yyyy");
+                    }
+                    else
+                    {
+                        DateTime? validDate = CheckIfValidDate(newHabitDate);
+
+                        while (validDate == null)
+                        {
+                            Console.WriteLine("Enter a valid date (MM/dd/yyyy)");
+                            newHabitDate = Console.ReadLine();
+                            validDate = CheckIfValidDate(newHabitDate);
+
+                        }
+                
+                    }
+                    updateQuery = @"UPDATE Habits SET date = $newHabitDate WHERE habitId = $targetHabitId";
+                    var updateDateParams = new Dictionary<string, object>
+                        { { "$targetHabitId", targetHabitId }, { "$newHabitDate", newHabitDate} };
+                    Execute(updateQuery, updateDateParams);
+                    break;
+                default:
+                    Console.WriteLine("Incorrect option");
+                    break;
+            }
+            
+            return "Habit Updated";
+            
+        }
+        
+        return "No Habits with this ID found";
+        
     }
 
     public string DeleteHabit()
@@ -184,10 +296,18 @@ public class DatabaseHelper
             targetHabitId = targetHabitIdresult;
             
         }
+        
+        string existsQuery = @"SELECT COUNT(*) FROM Habits WHERE habitId = $targetHabitId";
         var deleteParams = new Dictionary<string, object> { { "$targetHabitId", targetHabitId } };
-        Execute(query, deleteParams);
+        var count = ExecuteScalar(existsQuery, deleteParams);
+        if (count == 1)
+        {
+            Execute(query, deleteParams);
+            return "Habit Deleted";
+            
+        }
 
-        return "Habit Deleted";
+        return "No Habits with this ID found";
     }
     
     public void ExecuteReader(string sql)
@@ -206,10 +326,12 @@ public class DatabaseHelper
                     {
                         int habitId = reader.GetInt32(0);
                         string habitName = reader.GetString(1);
+                        string habitUnit = reader.GetString(2);
                         int habitQuantity = Convert.ToInt32(reader["habitQuantity"]);
-                        string date = reader.GetString(3);
+                        string date = reader.GetString(4);
+                        
 
-                        Console.WriteLine($"ID: {habitId}, Habit Name: {habitName}, Quantity: {habitQuantity}, Date: {date}");
+                        Console.WriteLine($"ID: {habitId}, Habit Name: {habitName}, Quantity: {habitQuantity} {habitUnit}, Date: {date}");
                     }
                 }
             }
